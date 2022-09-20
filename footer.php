42b8de4bbfdb90a7ccd2086413b0f29042af3438
satoshizen <s_shizen@jupiter.ocn.ne.jp>
2022-09-21 02:53:33 +0900

    function

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!-- フッター -->
+<footer class="footer">
+            <!-- ここにフッターが入ります -->
+            <small>Copyright🄫2022株式会社オーロラ物流<br>All Right Reserved.</small>
+        </footer>
+
+        <!-- JS -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <!--自作のJS-->
+        <script src="js/g-nav.js"></script>
+
+        <script src="./js/header.js"></script>
+    </body>
+</html>
---
+++ functions.php
@@ -0,0 +1,52 @@
+<?php
+// CSSの読み込み・JSの読み込み
+function mp_enqueue_files()
+{
+  // WordPress提供のjquery.jsを読み込まない
+  wp_deregister_script('jquery');
+
+  // jqueryの読み込み
+  wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js', "", "2.1.4", false);
+
+  // fontawesome 
+  wp_enqueue_script('fontawesome', 'http://kit.fontawesome.com/33d01a83f0.js', "", "", false);
+
+  // サイト共通のJS
+  wp_enqueue_script('hamburger', get_template_directory_uri() . '/js/hamburger.js', array('jquery'), '1.0', true);
+  if (is_front_page()) {
+    wp_enqueue_script('header', get_template_directory_uri() . '/js/header.js', array('jquery'), '1.0', true);
+  }
+
+
+  //サイト共通のCSSの読み込み
+  wp_enqueue_style('normalize', 'http://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css', '', '8.0.1');
+  wp_enqueue_style('mp_main', get_stylesheet_uri(), array('normalize'), '1.0');
+}
+add_action('wp_enqueue_scripts', 'mp_enqueue_files');
+
+// テーマのサポート
+function mp_theme_support()
+{
+  add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
+  add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+}
+add_action('after_setup_theme', 'mp_theme_support');
+
+//SVGをアップロード
+function mp_add_file_types_to_uploads($file_types)
+{
+
+  $new_filetypes = array();
+  $new_filetypes['svg'] = 'image/svg+xml';
+  $file_types = array_merge($file_types, $new_filetypes);
+
+  return $file_types;
+}
+add_action('upload_mimes', 'mp_add_file_types_to_uploads');
+
+function custom_theme_setup()
+{
+  add_theme_support('menus');
+}
+add_action('after_setup_theme', 'custom_theme_setup');
